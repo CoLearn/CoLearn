079c6ee27a18879bbd2543a9bf9f7b30db072262
Rahul Krishna <raahulkrsna@gmail.com>
2016-03-28 19:16:40 -0700

    Added a Add Schedule page

---
+++ CoLearn/AddScheduleViewController.swift
@@ -0,0 +1,35 @@
+//
+//  AddScheduleViewController.swift
+//  CoLearn
+//
+//  Created by Rahul Krishna Vasantham on 3/28/16.
+//  Copyright © 2016 CoLearn. All rights reserved.
+//
+
+import UIKit
+
+class AddScheduleViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
