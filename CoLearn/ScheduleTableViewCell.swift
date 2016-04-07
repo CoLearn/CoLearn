7b701927f927edd6b3917912f7a019731cb85ec9
SatyamJ <snjaiswa@asu.edu>
2016-04-06 20:34:43 -0700

    api calls for schedule page

--- CoLearn/ScheduleTableViewCell.swift
+++ CoLearn/ScheduleTableViewCell.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import Parse
 
 class ScheduleTableViewCell: UITableViewCell {
     
@@ -21,12 +22,54 @@ class ScheduleTableViewCell: UITableViewCell {
     
     @IBOutlet weak var requestNoteLabel: UILabel!
     
+    /*
     var schedule: Meeting?{
         didSet{
             languageLabel.text = schedule?.language
             meetingDatatime.text = schedule?.mtime
             instructorLabel.text = schedule?.instructor
             learnerLabel.text = schedule?.learner
+            requestNoteLabel.text = schedule?.requestNote
+        }
+    }*/
+    
+    var schedule: Schedule?{
+        didSet{
+            languageLabel.text = schedule?.language.getName()
+            
+            let dateFormatter = NSDateFormatter()
+            dateFormatter.dateStyle = .MediumStyle
+            dateFormatter.timeStyle = .NoStyle
+            dateFormatter.locale = NSLocale(localeIdentifier: "en_US")
+            
+            let date = dateFormatter.stringFromDate((schedule?.time)!)
+            
+            let timeFormatter = NSDateFormatter()
+            timeFormatter.dateFormat = "HH:mm"
+            let time = timeFormatter.stringFromDate((schedule?.time)!)
+            
+            meetingDatatime.text = "\(date) @\(time)"
+            
+            //meetingDatatime.text = "\(schedule?.time)"
+            
+            
+            instructorLabel.text = schedule?.instructor_id
+            //let instructor =
+            CoLearnClient.getUserDataFromDB((schedule?.instructor_id)!, success: { (user: PFObject?) -> () in
+                print(user)
+                self.instructorLabel.text = user!["name"] as? String
+                }) { (error: NSError?) -> () in
+                    print("Error getting the user info from db \(error?.localizedDescription)")
+            }
+            
+            learnerLabel.text = schedule?.user_id
+            CoLearnClient.getUserDataFromDB((schedule?.user_id)!, success: { (user: PFObject?) -> () in
+                print(user)
+                self.learnerLabel.text = user!["name"] as? String
+                }) { (error: NSError?) -> () in
+                    print("Error getting the user info from db \(error?.localizedDescription)")
+            }
+            
             requestNoteLabel.text = schedule?.requestNote
         }
     }
